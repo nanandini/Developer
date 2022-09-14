c07c43ba16b0175801cf1fe62d351af6d8380088
unknown <nandinin.xworkz@gmail.com>
2022-09-14 10:35:20 +0530

    hello

---
+++ IdentifyRunner.java
@@ -0,0 +1,37 @@
+class IdentifyRunner{
+	
+	public static void main(String[] self)
+	{
+		System.out.println("Identify Runner main method");
+		Identify.PrintName("Arya");
+		Identify.PrintNameAndEmail("Arya","Arya30@gmail.com");
+		Identify.PrintNameAndMobileNumber("Arya",9448958488L);
+		Identify.PrintNameAndGender("Arya",'M');
+	 
+	 
+	  for (byte person=0;person<=12;person++)
+	  {
+		  if(person==0)
+		  {
+			  Sytem.out.println("First persons details");
+		  }
+		  //-----1st
+		  if(person==1)
+		  {
+			  
+		  }
+		  
+	  }
+		  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	}
+	 
+	
+}
---
+++ Milestone.java
@@ -0,0 +1,75 @@
+class Milestone{
+	
+	static void ShowResponsibility()
+	{
+		  System.out.println("Excuting ShowResponsibility");
+		  
+		  int agewise=21;
+		  char gender='f';
+		   
+		if(agewise==10)
+		{
+			 System.out.println("its a age for going primary school");
+			
+		}
+		if(agewise==16)
+		{
+			  System.out.println("its a age ending with school level");
+			  return;
+		}
+		if(agewise==18)
+		{
+			  System.out.println("responsibility for taking voter ID legally");
+			  System.out.println("its a age for taking driving licence legally");
+			  
+			  if(gender=='f')
+			  {
+				  System.out.println("A girl marry leagally with this age");
+				  
+			  }
+			else
+			{
+				System.out.println("A girl cannot be marry leagally with this age");
+			}
+		}
+		     if(agewise==21)
+		     {
+			     System.out.println("A age for boys getting marry");
+				 System.out.println("its turning point");
+		     }
+		   if(agewise==21 || agewise==22 || agewise==23)
+			   
+		   {
+			  System.out.println("Becoming a Gratuaters");			  
+		   } 
+		   if(agewise==22 || agewise==23)
+		   {
+			   System.out.println("if not in the age of 21..will become Graduaters in the age of 22&23 "); 
+		   }
+		else
+		{
+			System.out.println("if not will not become a graduaters");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+   
+   	   
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
