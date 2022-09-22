e072fab968ef86541f751bdcece621369071af77
nanandini <113322369+nanandini@users.noreply.github.com>
2022-09-22 08:16:05 +0530

    Add files via upload

---
+++ ToothPaste.java
@@ -0,0 +1,95 @@
+class ToothPaste{
+	
+	
+	static String colour;
+	static String brand;
+    static int price;
+    static long mobileNo;
+	static String warning;
+    static String MFD;
+	static boolean extra;
+	static String type;
+	static int netWeight;
+	static char size;
+	static String ingredients;
+	static String expiryDate;
+	static String activeIngredients;
+	static String directions; 
+	
+	
+	static void setcolour(String value)
+	{
+		colour=value;
+	}
+	static String getcolour()
+	{
+		return colour;
+	}
+	static void setbrand(String value)
+	{
+		brand=value;
+	}
+	 static String getbrand()
+	 {
+		 return brand;
+	 }
+	 static void setprice(int value)
+	 {
+		 price=value;
+	 }
+	 static int getprice()
+	 {
+		 return price;
+	 }
+	 static void setmobileNo(long value)
+	 {
+		 mobileNo=value;
+	 }
+	 static long getmoblieNo()
+	 {
+		 return mobileNo;
+	 }
+	 static void setwarning(String value)
+	 {
+		 warning=value;
+	 }
+	 static String getwarning()
+	 {
+		 return warning;
+	 }
+	 static void setMFD(String value)
+	 {
+		 MFD=value;
+	 }
+	 static String getMFD()
+	 {
+		 return MFD;
+	 }
+	 static void setextra(boolean value)
+	 {
+		 extra=value;
+	 }
+	 static boolean getextra()
+	 {
+		 return extra;
+	 }
+	 static void settype(String value)
+	 {
+		 type=value;
+	 }
+	 static String gettype()
+	 {
+		 return type;
+	 }
+	 static void setnetWeight(int value)
+	 {
+		 netWeight=value
+	 }
+	 static int getnetWeight()
+	 {
+		 return netWeight;
+	 }
+	 static void 
+
+   	 
+}
